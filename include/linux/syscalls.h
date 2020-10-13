ef41768f531d7660576096c55ab228c371f9c9f4
ST-Saint <st.saint.wyy@gmail.com>
2020-10-13 23:12:56 +0800

    __arm64 syscall prefix

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1220,7 +1220,7 @@ asmlinkage long sys_mmap_pgoff(unsigned long addr, unsigned long len,
             unsigned long fd, unsigned long pgoff);
 asmlinkage long sys_old_mmap(struct mmap_arg_struct __user *arg);
 
-asmlinkage long sys_register_watchpoint(unsigned long addr);
+asmlinkage long __arm64_sys_register_watchpoint(unsigned long addr);
 
 /*
  * Not a real system call, but a placeholder for syscalls which are
