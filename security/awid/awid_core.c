eb9bce142de815a9fe70cca69b7e7511c4c01fa1
ST-Saint <st.saint.wyy@gmail.com>
2020-10-31 15:09:17 +0000

    copy to array offset

--- security/awid/awid_core.c
+++ security/awid/awid_core.c
@@ -173,9 +173,9 @@ SYSCALL_DEFINE4(register_watchpoint,
 	}
 	printk(KERN_INFO "register watchpoint on slot %d\n", slot);
 	hbp = register_wide_hw_breakpoint(&attr, awid_simple_handler, NULL);
-	copy_from_user(current->thread.debug.awid_hbp[slot], hbp,
+	copy_from_user(current->thread.debug.awid_hbp + slot, hbp,
 		       sizeof(struct perf_event));
-	printk(KERN_INFO "target slot %lx %lx\n",
+	printk(KERN_INFO "hbp %lx\ntarget slot %lx %lx\n", (unsigned long)hbp,
 	       (unsigned long)(current->thread.debug.awid_hbp + slot),
 	       (unsigned long)current->thread.debug.awid_hbp[slot]);
 	hbp = NULL;
