5591f32ab1a69a84e77b3eb1fb42e2ed8fc6a21e
ST-Saint <st.saint.wyy@gmail.com>
2021-05-01 14:02:38 +0800

    debug: move sys entry to awid_core

--- security/awid/awid_core.c
+++ security/awid/awid_core.c
@@ -31,6 +31,28 @@
 #include <net/sock.h>
 #include <linux/syscalls.h>
 
+typedef struct {
+	long (*open)(int, const char *path, int mode, umode_t);
+	int (*close)(unsigned int fd);
+	long (*write)(unsigned int fd, const char *data, size_t size);
+	long (*read)(unsigned int fd, char *buf, size_t size);
+	int (*printf)(const char *fmt, ...);
+	/* int (*scanf)(const char *fmt, ...); */
+} sysent_t;
+
+extern sysent_t syscalls;
+static const sysent_t sysentries = {
+	do_sys_open, /* */
+	ksys_close, /* */
+	ksys_write, /* */
+	ksys_read, /* */
+	printk, /* */
+	/* scanf /\* *\/ */
+};
+
+static const ELFSymbol_t exports[] = { { "syscalls", (void *)&sysentries } };
+static const ELFEnv_t env = { exports, sizeof(exports) / sizeof(*exports) };
+
 struct perf_event *hbp;
 struct perf_event **awid_hwps[ARM_MAX_WRP];
 
--- security/awid/loader_userdata.h
+++ security/awid/loader_userdata.h
@@ -2,31 +2,8 @@
 #define LOADER_USER_DATA_H
 
 #include "linux/types.h"
-#include "loader_config.h"
 #include <linux/unistd.h>
 #include <linux/syscalls.h>
-
-typedef struct {
-	long (*open)(int, const char *path, int mode, umode_t);
-	int (*close)(unsigned int fd);
-	long (*write)(unsigned int fd, const char *data, size_t size);
-	long (*read)(unsigned int fd, char *buf, size_t size);
-	int (*printf)(const char *fmt, ...);
-	/* int (*scanf)(const char *fmt, ...); */
-} sysent_t;
-
-extern sysent_t syscalls;
-static const sysent_t sysentries = {
-	do_sys_open, /* */
-	ksys_close, /* */
-	ksys_write, /* */
-	ksys_read, /* */
-	printk, /* */
-	/* scanf /\* *\/ */
-};
-
-static const ELFSymbol_t exports[] = { { "syscalls", (void *)&sysentries } };
-static const ELFEnv_t env = { exports, sizeof(exports) / sizeof(*exports) };
 
 typedef struct loader_env {
 	int fd;
